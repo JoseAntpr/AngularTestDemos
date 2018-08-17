a53ebe7869dfeacc46cdd4e15ccbe05e96e76686
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-08-17 12:47:33 +0200

    ADD: understand test lifecycle

--- src/app/basicos/classes/class.spec.ts
+++ src/app/basicos/classes/class.spec.ts
@@ -3,12 +3,42 @@ import { Player } from './class';
 
 describe('Test case for classes', () => {
 
+    // beforeAll()
+    // beforeEach()
+
+    // afterAll()
+    // afterEach()
+
+    let player = new Player();
+
+    beforeAll( () => {
+        console.log('beforeAll');
+    });
+
+    beforeEach( () => {
+        console.log('BeforeEach');
+        player = new Player();
+    });
+
+    afterAll(() => {
+        console.log('AfterAll');
+    });
+
+    afterEach(() => {
+        console.log('AfterEach');
+    });
+
     it('Should be return 80 hp if take 20 of damage', () => {
 
-        const player = new Player();
-        
         const res = player.takeDamage(20);
 
         expect(res).toBe(80);
+    });
+
+    it('Should be return 50 hp if take 50 of damage', () => {
+
+        const res = player.takeDamage(50);
+
+        expect(res).toBe(50);
     });
 });
