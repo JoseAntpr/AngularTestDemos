e9cabf40f654b28f9d28104aee9a8ac05ba1bf8d
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-08-19 14:00:47 +0200

    ADD: confirm exists specified routerLink

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,7 +2,7 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { By } from '@angular/platform-browser';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -26,6 +26,25 @@ describe('AppComponent', () => {
     const debugElement = fixture.debugElement.query( By.directive(RouterOutlet));
 
     expect(debugElement).not.toBeNull();
+  });
+
+  it('shlould have a link to hospital', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+
+    const debugElements = fixture.debugElement.queryAll( By.directive(RouterLinkWithHref));
+
+    console.log(debugElements);
+
+    let exists = false;
+
+    for ( const elem of debugElements) {
+      if ( elem.attributes['routerLink'] === '/hospital') {
+        exists = true;
+        break;
+      }
+    }
+
+    expect( exists ).toBeTruthy();
   });
 
 });
